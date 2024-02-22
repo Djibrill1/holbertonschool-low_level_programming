f8ade7b35dc48d20fe92591f45bd3a00b68d1c9d
Djibrill1 <djibrillmf@gmail.com>
2024-02-22 15:14:37 +0100

    Projet C-Hello, World

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("with proper grammar, but the outcome is a piece of art\n");
+    return 0;
+}
+
+gcc -Wall -o hello_world hello_world.c
+
+./hello_world
---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main(void) {
+    printf("Size of char: %ld byte(s)\n", sizeof(char));
+    printf("Size of int: %ld byte(s)\n", sizeof(int));
+    printf("Size of long int: %ld byte(s)\n", sizeof(long int));
+    printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
+    printf("Size of float: %ld byte(s)\n", sizeof(float));
+    printf("Size of double: %ld byte(s)\n", sizeof(double));
+    printf("Size of long double: %ld byte(s)\n", sizeof(long double));
+
+    return 0;
+}
+ 
+gcc -o taille_types taille_types.c
+
+./taille_types
