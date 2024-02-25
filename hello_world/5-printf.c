014b05ad9b6da497c407b185d15697b278ccf5b2
Djibrill1 <djibrillmf@gmail.com>
2024-02-26 00:02:25 +0100

    Projet

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,12 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always  (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
-
